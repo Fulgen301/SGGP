105838e5021f19ffa74cbb4e249e9265d0a8ac4c
Fulgen301 <tokmajigeorge@gmail.com>
2016-11-28 15:42:38 +0100

    Shield generator + Added shields with solidmask

---
+++ SGGPV30.c4d/Structures.c4d/Goa'Uld.c4d/Schildgenerator.c4d/Shield600.c4d/Script.c
@@ -0,0 +1,34 @@
+/* Schutzschild */
+
+#strict 2
+
+protected func Construction()
+{
+	SetSolidMask(0,0,GetDefWidth(GetID()), GetDefHeight(GetID()));
+}
+
+protected func OnDmg(int iDmg, int iType)
+{
+	if(iType != DMG_Explosion)
+	{
+		return 100;
+	}
+	return 75;
+}
+
+protected func Damage(int iDamage)
+{
+	GetEnergy() -= Min(iDamage/10, 50);
+}
+
+public func & GetEnergy()
+{
+	if(GetActionTarget()) return GetActionTarget()->LocalN("energy");
+}
+
+protected func LightningStrike()
+{
+	GetEnergy() += 10;
+}
+
+public func IsShield() { return true; }

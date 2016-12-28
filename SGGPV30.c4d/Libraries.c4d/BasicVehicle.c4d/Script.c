5171ccff7278f1f091efa9b87ff52913890b25ac
Markus Mittendrein <git@maxmitti.tk>
2016-12-28 17:26:23 +0100

    Localize Date and Time formatting in VehicleBase

--- SGGPV30.c4d/Libraries.c4d/BasicVehicle.c4d/Script.c
+++ SGGPV30.c4d/Libraries.c4d/BasicVehicle.c4d/Script.c
@@ -44,7 +44,7 @@ protected func ContainedDig(object pCaller)
 protected func MakeMenu(object pCaller)
 {
 	pPilot = pCaller;
-	CreateMenu(GetID(), pPilot, this, C4MN_Extra_None, Format("%04d-%02d-%02d %02d:%02d:%02d", GetSystemTime(), GetSystemTime(1), GetSystemTime(3),GetSystemTime(4), GetSystemTime(5), GetSystemTime(6)), 0, C4MN_Style_Dialog);
+	CreateMenu(GetID(), pPilot, this, C4MN_Extra_None, FormatN("$DateTimeFormat$", ["Y", "M", "D", "h", "m", "s"], [GetSystemTime(0), GetSystemTime(1), GetSystemTime(3), GetSystemTime(4), GetSystemTime(5), GetSystemTime(6)]), 0, C4MN_Style_Dialog);
 	AddMenuItem("$Status$", 0, NONE, pPilot);
 	AddMenuItem(Format("$Damage$: %v/%v", GetDamage(), MaxDamage()), 0, MEPU, pPilot);
 	AddMenuItem(Format("$Energy$: %v%%", energy), 0, MEPU, pPilot);

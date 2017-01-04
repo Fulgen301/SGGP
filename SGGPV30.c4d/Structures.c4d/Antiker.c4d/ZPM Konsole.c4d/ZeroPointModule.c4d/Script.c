84dd65299edbabf1300471bcc5e137b020ce5e75
Fulgen301 <tokmajigeorge@gmail.com>
2017-01-04 17:20:45 +0100

    ZPM2: Fix actions

--- SGGPV30.c4d/Structures.c4d/Antiker.c4d/ZPM Konsole.c4d/ZeroPointModule.c4d/Script.c
+++ SGGPV30.c4d/Structures.c4d/Antiker.c4d/ZPM Konsole.c4d/ZeroPointModule.c4d/Script.c
@@ -16,7 +16,7 @@ func Minus()
    timer--;
    return(1);
   }
-  if(FindContents(ZPM_))
+  if(FindContents(ZPM_) && FindContents(ZPM_)->GetAction() != "Depledet")
   {
    FindContents(ZPM_)->Minus();
    timer = 50;
@@ -43,17 +43,23 @@ protected func Collection2(object pZPM)
 	if(!pZPM) return;
 	if(pZPM && (pZPM->GetID() != ZPM_ || ContentsCount() > 2)) return pZPM->Exit();
 	SetAction(pZPM->GetAction());
+	Visible();
+}
+
+protected func RejectCollect()
+{
+	return !!Contents();
 }
 
 protected func Ejection(object pZPM)
 {
 	if(!pZPM || pZPM->GetID() != ZPM_) return;
-	ObjectSetAction(this, "Idle");
+	ObjectSetAction(this, "None");
+	Invisible();
 }
 
 public func SetAction(string szAction)
 {
-	Log(szAction);
 	if(Contents()) Contents()->SetAction(szAction, ...);
 	return _inherited(szAction, ...);
 }

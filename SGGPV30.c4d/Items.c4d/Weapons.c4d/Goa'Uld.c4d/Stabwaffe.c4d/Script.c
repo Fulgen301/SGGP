42064ff69920b54d279a414f0d79466c428d8462
Fulgen301 <tokmajigeorge@gmail.com>
2017-01-23 21:40:55 +0100

    STBW: Rework, new sounds

--- SGGPV30.c4d/Items.c4d/Weapons.c4d/Goa'Uld.c4d/Stabwaffe.c4d/Script.c
+++ SGGPV30.c4d/Items.c4d/Weapons.c4d/Goa'Uld.c4d/Stabwaffe.c4d/Script.c
@@ -6,20 +6,14 @@
 
 // Anzeige in der Hand
 public func HandSize() { return(800); }
-public func HandX()    { return(5000); }
-public func HandY()    { return(-1000); }
+public func HandX()    { return(7000); }
+public func HandY()    { return(-5000); }
 public func BarrelYOffset() { return(-2700); }
 public func GetRace()	{ return SG1_Goauld; }
 
-public func Initialize()
+public func OnAimStart()
 {
-  CreateContents(GONZ);
-  return _inherited();
-}
-
-public func Selection()
-{
-  Sound("staff_engage");
+  Sound("lancearme");
   return(1);
 }
 public func OnReload(int i)
@@ -37,7 +31,7 @@ public func FMData1(int data)
 
   if(data == FM_Reload)    return(50);
   if(data == FM_Recharge)  return(1);
-//  if(data == FM_Condition) return(!GetUpgrade(KLAS));
+  if(data == FM_Aim) 	   return(1);
 
   if(data == FM_Damage)    return(30);
 
@@ -46,10 +40,6 @@ public func FMData1(int data)
 
 public func Fire1()    // Projektilschuss
 { 
-  if(!FindContents(GONZ))
-  {
-   return 0;
-  }
   var user = GetUser();
   var angle = user->AimAngle(20) + RandomX(-1,+1);
   var x,y; user->WeaponEnd(x,y);
@@ -62,76 +52,8 @@ public func Fire1()    // Projektilschuss
   BulletCasing(dir*6,3,-dir*5,-20,5);
 
   // Sound
-  Sound("staff_weapon",0,ammo);
+  Sound("LaunchSW",0,ammo);
   Reload();
-}
-
-public func ContextMenu(object pCaller)
-{
-  [$Menu$|Image=STBW]
-  Menu(pCaller);
-}
-
-public func HasCell()
-{
-  if(FindContents(GONZ))
-  {
-   return 1;
-  }
-  else
-  {
-   return 0;
-  }
-}
-
-public func Reload()
-{
-  if(!FindContents(GONZ))
-  {
-   return 0;
-  }
-  return _inherited();
-}
-
-public func Menu(object pCaller)
-{
-  CreateMenu(STBW,pCaller,this(),0,"Stabwaffe",0,1);
-  if(FindContents(GONZ))
-  {
-   AddMenuItem("Zelle ausbauen","PutTake",GONZ,pCaller);
-  }
-  else
-  {
-   AddMenuItem("Zelle einbauen","PutTake",GONZ,pCaller);
-  }
-  return 1;
-}
-
-public func PutTake()
-{
-  Sound("Connect");
-  if(FindContents(GONZ))
-  {
-   FindContents(GONZ)->Enter(Contained());
-   if(IsReloading()) RemoveEffect("Reload", this());
-   if(IsRecharging()) RemoveEffect("Recharge", this());
-   var ammoid = GetFMData(FM_AmmoID);
-   var ammoamount = GetAmmo(ammoid, this());
-   DoAmmo(ammoid, -ammoamount, this());
-  }
-  else
-  {
-   if(!FindContents(GONZ,Contained()))
-   {
-    Message("Keine Zelle für den Einbau vorhanden!",this());
-    return 1;
-   }
-   else
-   {
-    FindContents(GONZ,Contained())->Enter(this());
-   }
-  }
-  return 1;
 }
 
 func IsGoauldWeapon() { return(true); }

d010ee57e6f3fe7aec8b2fe1528d841721b92493
Fulgen301 <tokmajigeorge@gmail.com>
2017-02-05 21:50:13 +0100

    Removing points for iris

--- SGGPV30.c4d/System.c4g/Appendto-Structures.c
+++ SGGPV30.c4d/System.c4g/Appendto-Structures.c
@@ -7,7 +7,7 @@
 protected func Completion()
 {
 	_inherited(...);
-	if((GetCategory(this) & C4D_Structure) && !this->~IsRepli())
+	if(this && (GetCategory(this) & C4D_Structure) && !this->~IsRepli() && GetID(this) != SGIR)
 	{
 		DoPoints(this->GetOwner(), 10, SGGP_SettlePoints);
 		if(FindObject2(Find_Distance(200),Find_Func("IsPartOfAtlantis")))

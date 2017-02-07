5476ac0bbdbe05320b4f69ff9467c60c59eecc27
Fulgen301 <tokmajigeorge@gmail.com>
2017-02-07 14:07:04 +0100

    Planetensystem: Add "#include SGGP"

--- SGGPV30.c4f/Siedeln.c4f/SGGPV30Planetensystem.c4s/Script.c
+++ SGGPV30.c4f/Siedeln.c4f/SGGPV30Planetensystem.c4s/Script.c
@@ -1,6 +1,7 @@
 /*-- SG Welt --*/
 
 #strict 2
+#include SGGP
 
 static fowTools;
 static xpositions;
@@ -8,13 +9,14 @@ static ypositions_ranges;
 
 func Initialize()
 {
+	_inherited(...);
   CreateGate(SGR3, SGDG, 515, 298, -1, [8, 13, 7, 3, 10, 2, 9]);
   CreateGate(SGR2, SGAG, 2007, 1570, -1, [3, 7, 13, 10, 9, 15, 11]);
   CreateGate(SGR3, SGDG, 2507, 3131, -1, [8, 9, 13, 14, 15, 12, 4]);
   CreateGate(SGR2, STGT, 347, 4151, -1, [9, 2, 6, 12, 15, 4, 8]);
   CreateGate(SGR2, SGAG, 2010, 5321, -1, [13, 10, 7, 6, 14, 8, 12]);
   CreateGate(SGR3, SGDG, 3504, 5983, -1, [9, 10, 15, 2, 3, 13, 12]);
-  CreateGate(SGR4, STGT, 1600, 7005, -1, [14, 2, 8, 10, 3, 11, 9]);
+  CreateGate(SGR3, STGT, 1600, 7005, -1, [14, 2, 8, 10, 3, 11, 9]);
   
   CreateObject(DHD_, 1514, 7000, -1);
   CreateObject(DHD_, 272, 4148, -1);
@@ -32,6 +34,7 @@ func Initialize()
 
 func InitializePlayer(int plr, int x, int y, object base, int team)
 {
+	_inherited(plr, x, y, base, team, ...);
   var spawnpoints = [[1950, 240], [3530, 1510], [1800, 2990], [2040, 4100], [1850, 5310], [3400, 5960], [1710, 6990]];
   var startMaterial = [[FLAG, 1], [CNKT, 1]];
 

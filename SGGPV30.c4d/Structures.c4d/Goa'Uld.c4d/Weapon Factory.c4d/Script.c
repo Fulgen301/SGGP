2026d4e6586950310dc68be50597f585628d613f
Fulgen301 <tokmajigeorge@gmail.com>
2017-03-25 18:25:05 +0100

    GOWF: Change code

--- SGGPV30.c4d/Structures.c4d/Goa'Uld.c4d/Weapon Factory.c4d/Script.c
+++ SGGPV30.c4d/Structures.c4d/Goa'Uld.c4d/Weapon Factory.c4d/Script.c
@@ -1,40 +1,22 @@
 /*-- Goa'Uld Waffenfabrik --*/
 
-#strict
+#strict 2
 
 /* Nutzt die Grundfunktionalität der Werkstatt. */
 
 #include WRKS
 #include BAS9
 
-/* Produkteigenschaften (überladen) */
-public func ProductType () {return(C4D_Object());}
-
-
-public func ProductCondition() 
+private func MenuProduction(pCaller)
 {
-  return("IsGoauldWeapon");
-}
-
-public func ProductCondition2() 
-{
-  return("IsAmmoPacket");
-}
-
-/* Produktion */
-
-private func MenuProduction(pCaller) 
-{
-  // Menü erzeugen und mit Bauplänen des Spielers füllen
-  CreateMenu(GetID(this()),pCaller,this(),1,"$TxtNoconstructionplans$");
-  for(var i=0,idKnowledge; idKnowledge=GetPlrKnowledge(GetOwner(pCaller),0,i,ProductType ()); ++i)
-    if (DefinitionCall (idKnowledge, ProductCondition ()))
-      AddMenuItem("$TxtProductions$", "SelectProduction", idKnowledge, pCaller, 0, pCaller);
-      
-  for(var i=0,idKnowledge; idKnowledge=GetPlrKnowledge(GetOwner(pCaller),0,i,ProductType ()); ++i)
-    if (DefinitionCall (idKnowledge, ProductCondition2 ()))
-      AddMenuItem("$TxtProductions$", "SelectProduction", idKnowledge, pCaller, 0, pCaller);
-  return(1);
+	CreateMenu(GetID(), pCaller, this, 1, "$TxtNoconstructionplans$");
+	for(var i = 0, Def; Def = GetPlrKnowledge(GetOwner(pCaller), 0, i, C4D_Object); i++)
+	{
+		if(Def && ((Def->~GetRace() & SG1_Goauld) || (Def->~GetRace() & SG_All)))
+		{
+			AddMenuItem("$TxtProductions$", "SelectProduction", Def, pCaller, 0, pCaller);
+		}
+	}
 }
 
 /* Türsteuerung */
